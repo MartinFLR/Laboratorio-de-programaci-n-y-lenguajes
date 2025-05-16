f56ce09c856971dd7250e79419eace88917f07a0
MartinFLR <132178630+MartinFLR@users.noreply.github.com>
2025-05-16 15:08:11 -0300

    update

--- parciales/tests/test.js
+++ parciales/tests/test.js
@@ -1,12 +1,47 @@
 
 
 document.addEventListener("DOMContentLoaded",function(){
-    let texto = "perro";
+    let texto = "pero";
     let parrafo = document.getElementById("test");
 
-    console.log(texto.matchAll(/r/g))
-
-    
-
+    matchAll(texto);
+    match(texto);
 })
+
+function matchAll(texto){
+    
+    console.log("MATCH ALL");
+
+    //Ejemplo de uso de matchAll con forOf
+    //devuelve un iterador
+    let iterador = texto.matchAll(/r/g)
+
+    for (const elem of iterador) {
+        console.log(elem[0])
+    }
+
+
+    //Ejemplo de uso de matchAll con array
+
+    console.log("CON ARRAYS")
+    let arrayIterador = Array.from(texto.matchAll(/r/g));
+    arrayIterador.forEach((elem) => {
+        console.log(elem[0])
+    })
+}
+
+function match(texto){
+    console.log("MATCH");
+
+    //Ejemplo de uso de match
+    //devuelve un array
+
+    let array = texto.match(/r/g);
+
+    if(!array){
+        console.log("no hay coincidencias")
+    }else{
+        console.log(array)
+    }
+}
 
