3f97879d86c25f9a999b82817fd0c08d6dca3152
MartinFLR <132178630+MartinFLR@users.noreply.github.com>
2025-05-16 14:46:19 -0300

    cheatsheet

---
+++ parciales/tests/test.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    let texto = "perro";
+    let parrafo = document.getElementById("test");
+
+    console.log(texto.matchAll(/r/g))
+
+    
+
+})
+
