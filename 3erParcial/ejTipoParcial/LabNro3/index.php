1d52a16a906943b0b1761130af158dc8e46f1117
MartinFLR <132178630+MartinFLR@users.noreply.github.com>
2025-06-25 03:25:40 -0300

    llbb

---
+++ 3erParcial/ejTipoParcial/LabNro3/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LabNro3</title>
+    <link rel="stylesheet" href="styles.css" />
+
+</head>
+<body>
+    <header>
+        <h1>LabNro3</h1>
+    </header>
+    <main>
+        <h2>Información</h2>
+        <div id="informacion"></div>  
+
+        <h2>Filtros</h2>
+            <select id="ciudadesOrigen" name="ciudadesOrigen">
+                <option value="">Seleccione una ciudad</option>
+            </select>
+            <select id="ciudadesDestino" name="ciudadesDestino">
+                <option value="">Seleccione una ciudad</option>
+            </select>
+
+        <h2>Resultado</h2>
+        <div id="resultado"></div>
+
+    </main>
+
+    <footer>
+        <p>Martín Leonardo Flores</p>
+    </footer>
+
+    <script src="js/main.js"></script>
+</body>
+</html>
